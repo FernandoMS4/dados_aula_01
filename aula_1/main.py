fec328dbdf67e06afed0abeea9244398ffa8b8ee
FernandoMS4 <fernando381046@gmail.com>
2024-12-10 09:35:16 -0300

    contruction: Primeiro exercício

---
+++ aula_1/main.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+salario = float(input("Digite seu salário "))
+bonus = float(input("Digite o valor de bonus recebido ex 1.8: "))
+
+print(f"{nome} obrigado, o valor de bonus foi de {1000 + (salario * bonus)}")
